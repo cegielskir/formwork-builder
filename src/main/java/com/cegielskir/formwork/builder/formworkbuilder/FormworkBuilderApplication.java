57f87d5d2502be6ac501c24bc0539528ac931723
Rafał Cegielski <cegielski.rafaal@gmail.com>
2019-02-28 11:54:49 +0100

    Initial commit

---
+++ src/main/java/com/cegielskir/formwork/builder/formworkbuilder/FormworkBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.cegielskir.formwork.builder.formworkbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormworkBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormworkBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cegielskir/formwork/builder/formworkbuilder/FormworkBuilderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cegielskir.formwork.builder.formworkbuilder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FormworkBuilderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
