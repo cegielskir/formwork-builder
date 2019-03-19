5b6d4ea7057a5db809bf67de476f331405d722a4
Rafał Cegielski <cegielski.rafaal@gmail.com>
2019-03-19 20:39:42 +0100

    Result as JSON

--- src/main/java/com/cegielskir/formwork/builder/controller/FormworkController.java
+++ src/main/java/com/cegielskir/formwork/builder/controller/FormworkController.java
@@ -22,7 +22,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/formwork")
 public class FormworkController {
 
--- src/main/java/com/cegielskir/formwork/builder/controller/FormworkRestController.java
+++ src/main/java/com/cegielskir/formwork/builder/controller/FormworkRestController.java
@@ -1,4 +1,28 @@
 package com.cegielskir.formwork.builder.controller;
 
+import com.cegielskir.formwork.builder.entity.Formwork;
+import com.cegielskir.formwork.builder.service.FormworkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/formwork")
 public class FormworkRestController {
+
+    @Autowired
+    FormworkService formworkService;
+
+    @GetMapping("/get_result/{formworkId}")
+    public String getResultAsJSON(@PathVariable("formworkId") int id, Model model){
+        Formwork formwork = formworkService.getById(id);
+        return formwork.getFormworkProject().getResultJSON();
+
+    }
+
+
+
 }
